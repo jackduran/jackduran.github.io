5338f7e4fd9eb4143b6e11e9d4bdd7083e004034
jackduran <60995597+jackduran@users.noreply.github.com>
2021-05-06 03:53:22 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,22 +1,25 @@
 var config = {
-    style: 'mapbox://styles/mapbox/satellite-streets-v11',
-    accessToken: 'pk.eyJ1IjoiamFja2R1cmFuIiwiYSI6ImNrbG93OXVobTA3c3Mydm1tZWI3Y3hnajUifQ.DKkhIm2r4wePWQcA09RrKw',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoiZHVyYWoxODMiLCJhIjoiY2tvY2t5M3c1MDZ5YTJ4cW9idHczZmpwayJ9.QW4k4sikwxvk0tcp0GE0xg',
     showMarkers: true,
     markerColor: '#3FB1CE',
-    theme: 'light',
-    use3dTerrain: false,
+    theme: 'dark',
+    use3dTerrain: true,
+    title: 'The Title Text of this Story',
+    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    byline: 'By a Digital Storyteller',
     footer: 'Source: source citations, etc.',
     chapters: [
         {
-            id: 'mexicoseesaw',
+            id: 'newjersey',
             alignment: 'left',
             hidden: false,
-            title: 'Artists Briefly Bridge US-Mexico Border',
-            image: './path/to/image/source.png',
-            description: 'In a rare moment of celebration at the US-Mexico border: children from both countries played together on pink seesaws straddling the steel border fence separating El Paso, Texas, and Juárez, Mexico. The almost surreally joyous scene was a temporary art piece titled Teeter-Totter Wall, meant to foster a sense of unity between the two nations. The art project, which has been a media sensation, is the work of architecture studio Rael San Fratello, a partnership between San Jose State interior design faculty member Virginia San Fratello and UC Berkeley architecture professor Ronald Rael, author of the 2017 book Borderwall as Architecture: A Manifesto for the U.S.-Mexico Boundary. Read More: www.jack-duran.com',
+            title: 'New Jersey',
+            image: '',
+            description: 'The Garden State',
             location: {
-                center: [-104.70429, 30.20707],
-                zoom: 19.04,
+                center: [-74.72320, 40.11608],
+                zoom: 7.25,
                 pitch: 45.00,
                 bearing: 0
             },
@@ -38,17 +41,17 @@ var config = {
             ]
         },
         {
-            id: 'calhouncounty',
+            id: 'dr',
             alignment: 'right',
             hidden: false,
-            title: 'Organizer Infiltrates ICE Facility',
-            image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            title: 'Dominican Republic',
+            image: '',
+            description: 'Quisqueya La Beya',
             location: {
-                center: [-85.00456, 42.24652],
-                zoom: 11.13,
+                center: [-70.11537, 18.70583],
+                zoom: 7.48,
                 pitch: 45.00,
-                bearing: -43.2
+                bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
